5809c8b90504bb668d1a866af1c5485d81742944
40501779 <40501779@live.napier.ac.uk>
2022-03-23 16:19:29 +0630

    added codes for getting salary information by role

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -25,10 +25,13 @@ public class App {
 //        a.displayEmployee(emp);
 
         // Extract employee salary information
-        ArrayList<Employee> employees = a.getAllSalaries();
+//        ArrayList<Employee> employees = a.getAllSalaries();
 
         // Test the size of the returned data - should be 240124
 //        System.out.println(employees.size());
+
+        // Extract employee salary information by role
+        ArrayList<Employee> employees = a.getAllSalariesByRole();
 
         // Print a list of employees
         a.printSalaries(employees);
@@ -183,6 +186,49 @@ public class App {
                     String.format("%-10s %-15s %-20s %-8s",
                             emp.emp_no, emp.first_name, emp.last_name, emp.salary);
             System.out.println(emp_string);
+        }
+    }
+
+    /**
+     * Gets all the current employees and salaries by role.
+     * @return A list of all employees and salaries by role, or null if there is an error.
+     */
+    public ArrayList<Employee> getAllSalariesByRole()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary "
+                        + "FROM employees, salaries, titles "
+                        + "WHERE employees.emp_no = salaries.emp_no"
+                        + "AND employees.emp_no = titles.emp_no "
+                        + "AND salaries.to_date = '9999-01-01' "
+                        + "AND titles.to_date = '9999-01-01' "
+                        + "AND titles.title = '<title>' "
+                        + "ORDER BY employees.emp_no ASC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<Employee> employees = new ArrayList<Employee>();
+            while (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("employees.emp_no");
+                emp.first_name = rset.getString("employees.first_name");
+                emp.last_name = rset.getString("employees.last_name");
+                emp.salary = rset.getInt("salaries.salary");
+                employees.add(emp);
+            }
+            return employees;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get salary details");
+            return null;
         }
     }
 }
