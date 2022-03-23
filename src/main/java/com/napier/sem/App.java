cc1203519065b300c76fdc0d024ed86dc0a20872
40501779 <40501779@live.napier.ac.uk>
2022-03-23 14:31:17 +0630

    added codes for printing salary information

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -18,17 +18,20 @@ public class App {
         // Connect to database
         a.connect();
 
-//        // Get Employee
+        // Get Employee
 //        Employee emp = a.getEmployee(255530);
-//
-//        // Display results
+
+        // Display results
 //        a.displayEmployee(emp);
 
         // Extract employee salary information
         ArrayList<Employee> employees = a.getAllSalaries();
 
         // Test the size of the returned data - should be 240124
-        System.out.println(employees.size());
+//        System.out.println(employees.size());
+
+        // Print a list of employees
+        a.printSalaries(employees);
 
         // Disconnect from database
         a.disconnect();
@@ -162,6 +165,24 @@ public class App {
             System.out.println(e.getMessage());
             System.out.println("Failed to get salary details");
             return null;
+        }
+    }
+
+    /**
+     * Prints a list of employees.
+     * @param employees The list of employees to print.
+     */
+    public void printSalaries(ArrayList<Employee> employees)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Loop over all employees in the list
+        for (Employee emp : employees)
+        {
+            String emp_string =
+                    String.format("%-10s %-15s %-20s %-8s",
+                            emp.emp_no, emp.first_name, emp.last_name, emp.salary);
+            System.out.println(emp_string);
         }
     }
 }
